d996b5f24c1be1e10ed99a326febcc15977204a1
JannerPareja <janpar81@gmail.com>
2023-06-09 16:10:38 -0500

    Carga del proyecto a Github

---
+++ js/cities.js
@@ -0,0 +1,26 @@
+var cities = [
+    {
+        "name": "Armenia",
+        "code": 01
+    },
+    {
+        "name": "Barranquilla",
+        "code": 02
+    },
+    {
+        "name": "Bogota",
+        "code": 03
+    },
+    {
+        "name": "Cartagena",
+        "code": 04
+    },
+    {
+        "name": "Medellín",
+        "code": 05
+    },
+    {
+        "name": "Valledupar",
+        "code": 06
+    }
+];
